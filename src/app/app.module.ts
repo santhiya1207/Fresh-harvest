e93b23fe74a5844b59b05ca66312736a8ca28cb3
Nithis Selvam <nithis.selvam@zucisystems.com>
2023-04-08 18:40:25 +0530

    homePage initial setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
+    HeaderComponent,
  
   ],
   imports: [
