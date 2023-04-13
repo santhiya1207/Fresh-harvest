3378a21b618cadee8e3048ed22ad2c5cd2f5b175
Nithis Selvam <nithis.selvam@zucisystems.com>
2023-04-13 20:57:19 +0530

    loginpage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
+import { LoginformComponent } from './loginform/loginform.component';
+
 
 const routes: Routes = [
-  
-  {path:'home', component: HomePageComponent}
+  {path:'home', component:HomePageComponent},
+  {path:'login', component:LoginformComponent},
+  {path:'',redirectTo:'login', pathMatch:'full'}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
-// import { FormsModule }   from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import {FormsModule} from '@angular/forms';
+import { FormsModule,ReactiveFormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { HeaderComponent } from './header/header.component';
-import { LoginpageComponent } from './loginpage/loginpage.component';
+
+import { LoginformComponent } from './loginform/loginform.component';
 
 
 
@@ -16,13 +16,13 @@ import { LoginpageComponent } from './loginpage/loginpage.component';
     AppComponent,
     HomePageComponent,
     HeaderComponent,
-    LoginpageComponent,
+    LoginformComponent
      
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,ReactiveFormsModule,
  
   
   ],
