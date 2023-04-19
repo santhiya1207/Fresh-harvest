86b0c7ca406d82b152040a1ff56425d03c2ef9b7
santhiya1207 <santhiyaselvan12@gmail.com>
2023-04-19 14:43:28 +0530

    Cart Count

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, HostListener } from "@angular/core";
+import { CartServiceService } from "../service/cart-service.service";
 
 @Component({
   selector: "app-header",
@@ -6,6 +7,17 @@ import { Component, HostListener } from "@angular/core";
   styleUrls: ["./header.component.css"],
 })
 export class HeaderComponent {
+
+  counters: number[] = [];
+  cartItems: any[] = [];
+
+  constructor(private cartService:CartServiceService){
+    this.cartItems = this.cartService.getItems();
+    this.counters = this.cartService.getCounters();
+    console.log("fgrr", this.counters.length);
+  }
+
+  
 
   logsrc = "/assets/logo.png";
 
